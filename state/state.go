8530a82dca158e7b9ce23bd8e596ee05a9f48ead
Brian Hicks <brian@brianthicks.com>
2015-08-19 12:27:08 -0700

    state: fill out types

--- state/state.go
+++ state/state.go
@@ -10,11 +10,19 @@ var (
 )
 
 type Task struct {
-	ID string
+	ID           string            `json:"id"`
+	SubscribesTo []string          `json:"subscribesTo"`
+	Image        string            `json:"image"`
+	Env          map[string]string `json:"env"`
+	CPU          float64           `json:"cpu"`
+	Mem          float64           `json:"mem"`
 }
 
 type Event struct {
-	ID string
+	ID      string            `json:"id"`
+	Type    string            `json:"type"`
+	Payload []byte            `json:"payload"`
+	Meta    map[string]string `json:"meta"`
 }
 
 type TaskStorer interface {

b1a4067e23568cd743f6ca61f5e7f75e2873097b
Brian Hicks <brian@brianthicks.com>
2015-08-19 10:58:24 -0700

    catch up git to my brain

--- cmd/reflex/main.go
+++ cmd/reflex/main.go
@@ -2,9 +2,21 @@ package main
 
 import (
 	"github.com/asteris-llc/reflex/http"
+	"github.com/asteris-llc/reflex/state"
+	"github.com/hashicorp/consul/api"
 )
 
 func main() {
-	api := http.API{}
+	client, err := api.NewClient(&api.Config{
+		Address: "localhost:8500",
+		Scheme:  "http",
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	store := state.NewConsulStore("reflex", client)
+
+	api := http.NewAPI(store)
 	api.Serve("localhost:4000")
 }
--- http/tasks.go
+++ http/tasks.go
@@ -1,14 +1,31 @@
 package http
 
 import (
+	"encoding/json"
+	"github.com/asteris-llc/reflex/state"
 	"github.com/gorilla/mux"
 	"net/http"
 )
 
-type TasksHandler struct{}
+type TasksHandler struct {
+	store state.TaskStorer
+}
 
 func (t *TasksHandler) List(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNoContent)
+	tasks, err := t.store.List()
+	if err != nil {
+		HandleError(err, w)
+		return
+	}
+
+	blob, err := json.Marshal(tasks)
+	if err != nil {
+		HandleError(err, w)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(blob)
 }
 
 func (t *TasksHandler) Get(w http.ResponseWriter, r *http.Request) {

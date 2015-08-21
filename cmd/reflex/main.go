898d423cc970b82f4ad075b0eb867b02a5814b7d
Brian Hicks <brian@brianthicks.com>
2015-08-21 11:25:00 -0700

    cmd(reflex): set debug level (for now)

--- cmd/reflex/main.go
+++ cmd/reflex/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"github.com/Sirupsen/logrus"
 	"github.com/asteris-llc/reflex/reflex"
 )
 
 func main() {
+	logrus.SetLevel(logrus.DebugLevel)
+
 	r, err := reflex.New(&reflex.Options{
 		Address: ":4000",
 	})
